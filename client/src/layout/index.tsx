ddee99f88c241cb795583c566db7adcb5e82c1c1
Martin Johansen <martin.johansen.98@outlook.com>
2024-05-21 17:40:00 +0200

    chore: Add loading component to layout and fetch user quizzes in profile page

--- client/src/layout/index.tsx
+++ client/src/layout/index.tsx
@@ -8,12 +8,20 @@ import Footer from './footer';
 import Promotion from './promote';
 import Cookies from './cookies';
 import { QuizProvider } from '../context/quizContext';
+import { useAuth0 } from '@auth0/auth0-react';
+import Loading from './loading'; 
 
 const Layout = () => {
+    const { isLoading } = useAuth0();
+
     const [sidebarOpen, setSidebarOpen] = useState(false);
 
     function openSidebar(): void {
         setSidebarOpen(!sidebarOpen);
+    }
+
+    if (isLoading) {
+        return <Loading />;
     }
 
     return (
--- client/src/pages/profile/profile.tsx
+++ client/src/pages/profile/profile.tsx
@@ -1,11 +1,38 @@
+import { useState, useEffect } from 'react';
 import Container from '../../layout/container'
 import Headline from '../../components/headline';
 import { useAuth0 } from '@auth0/auth0-react';
 import Empty from './components/empty';
 import { Strong } from '../../components/text';
+import axios from 'axios';
+import { IQuiz } from '../../interfaces/quiz';
+import Preview from './components/preview';
 
 const ProfilePage = () => {
     const { user } = useAuth0();
+    const [quizzes, setQuizzes] = useState([] as IQuiz[]);
+
+    useEffect(() => {
+        if (!user?.sub) {
+            return;
+        }
+        const fetchQuizzes = async () => {
+            try {
+
+                const response = await axios.get('/api/search/myquiz', {
+                    params: {
+                        ownerId: '66460b784901e7613d6b5663',
+                    },
+                });
+
+                setQuizzes(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchQuizzes();
+    }, [user]);
 
     return (
         <Container>
@@ -15,10 +42,19 @@ const ProfilePage = () => {
             </div>
 
             {/* Content */}
-            <div className="mt-10">
-                <Strong>Quizzes</Strong>
-                <Empty className="mt-4" />
-            </div>
+            {
+                quizzes.length === 0 ? (
+                    <div className="mt-10">
+                        <Strong>Quizzes</Strong>
+                        <Empty className="mt-4" />
+                    </div>
+                ) : (
+                    <div className="mt-10">
+                        <Strong>Quizzes</Strong>
+                        <Preview quizzes={quizzes} />
+                    </div>
+                )
+            }
         </Container>
     );
 };
